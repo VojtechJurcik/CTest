c147adbab277f1f354a70982536b1a8858f90ace
Vojtech Jurcik <vojtech.jurcik@gmail.com>
2022-11-25 13:38:16 +0100

    test

--- a.c
+++ a.c
@@ -1,8 +1,20 @@
-int* a = new int;
-for (int i = 0; i < 10000; i++)
+char *banner;
+
+void
+output_report (int nr)
 {
-    int* b = a; // we copy the address held by a
-    delete b; // uh oh we call delete on that same address again
-    b = new int; // put new memory in b, this does nothing to a
-} // leak here as b goes out of scope and we no longer have the address it held
+  puts (banner);
+  printf ("Number: %d\n", nr);
+  printf ("\n");
+}
+
+int
+main
+{
+  banner = create_banner ();
+  for (int i = 1; i <= 3; i++)
+    output_report (i);
+
+  return 0;
+}
 
